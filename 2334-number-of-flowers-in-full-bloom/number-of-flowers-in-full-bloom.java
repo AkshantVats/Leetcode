ac8e93f189d3ca484c89ffac938c276340b8033b
Akshant Vats <Akshantvats@outlook.com>
2024-07-19 17:46:20 +0530

    Time: 38 ms (89.52%) | Memory: 80.6 MB (14.45%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.java
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.java
@@ -1,40 +1,48 @@
-
-import java.util.Collection;
 class Solution {
     public int[] fullBloomFlowers(int[][] flowers, int[] people) {
-        List<Integer> starts = new ArrayList<>();
-        List<Integer> ends = new ArrayList<>();
+        int n = flowers.length;
+        int[] start = new int[n];
+        int[] end = new int[n];
 
-        for(int[] flower : flowers){
-            starts.add(flower[0]);
-            ends.add(flower[1] + 1);
+        for(int i = 0; i < n; i++) {
+            start[i] = flowers[i][0];
+            end[i] = flowers[i][1];
         }
 
-        Collections.sort(starts);
-        Collections.sort(ends);
+        Arrays.sort(start);
+        Arrays.sort(end);
+
         int[] ans = new int[people.length];
-        
-        for(int index = 0; index < people.length; index++ ){
-            int person = people[index];
-            int i = binarySearch(starts, person);
-            int j = binarySearch(ends, person);
-            ans[index] = i - j;
+
+        for(int i = 0; i < people.length; i++) {
+            // very first flower blooms after person time people[i]
+            int x = binarySearch(start, people[i] + 1); 
+
+            // last flower blooms 
+            int y = binarySearch(end, people[i]);
+            ans[i] = x - y;
         }
+
         return ans;
     }
 
-    public int binarySearch(List<Integer> arr, int target){
-        int left = 0;
-        int right = arr.size();
-        while(left < right){
-            int mid = (left + right) / 2;
-            if(target < arr.get(mid)){
-                right = mid;
-            }
-            else{
-                left = mid + 1;
+    int binarySearch(int[] arr, int target) {
+        int ans = arr.length;
+
+        int lo = 0;
+        int hi = arr.length - 1;
+
+        while(lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            if(arr[mid] >= target) {
+                ans = mid;
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
             }
         }
-        return left;
+
+        return ans;
     }
 }
